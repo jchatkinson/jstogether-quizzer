d498f1b743011f56c287f031ff6b1b760b60bcce
Jeremy Atkinson <jchatkinson@gmail.com>
2015-08-15 11:18:21 -0700

    api calls not working, switched to hard coded question for now

--- app.js
+++ app.js
@@ -17,15 +17,88 @@ quizzer.controller('quizCtrl', function (questionFactory) {
     q.score = 0;
     q.numQuestions = 4;
     q.showform = true;
+    q.questions = [{
+        "id": 16479,
+        "answer": "Napoleon",
+        "question": "The Bata Shoe Museum in Toronto features a pair of black silk socks worn by this emperor on St. Helena",
+        "value": 100,
+        "airdate": "1997-05-09T12:00:00.000Z",
+        "created_at": "2014-02-11T22:55:45.126Z",
+        "updated_at": "2014-02-11T22:55:45.126Z",
+        "category_id": 539,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+            "id": 539,
+            "title": "museums",
+            "created_at": "2014-02-11T22:49:06.194Z",
+            "updated_at": "2014-02-11T22:49:06.194Z",
+            "clues_count": 160
+        }
+    }, {
+        "id": 16480,
+        "answer": "Hospital",
+        "question": "(Big white \"H\" on a blue background)",
+        "value": 100,
+        "airdate": "1997-05-09T12:00:00.000Z",
+        "created_at": "2014-02-11T22:55:45.139Z",
+        "updated_at": "2014-02-11T22:55:45.139Z",
+        "category_id": 1715,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+            "id": 1715,
+            "title": "international signs \u0026 symbols",
+            "created_at": "2014-02-11T22:55:45.051Z",
+            "updated_at": "2014-02-11T22:55:45.051Z",
+            "clues_count": 5
+        }
+    }, {
+        "id": 16481,
+        "answer": "eyebrows",
+        "question": "A German woman may pluck these, her augenbrauen",
+        "value": 200,
+        "airdate": "1997-05-09T12:00:00.000Z",
+        "created_at": "2014-02-11T22:55:45.153Z",
+        "updated_at": "2014-02-11T22:55:45.153Z",
+        "category_id": 1714,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+            "id": 1714,
+            "title": "foreign anatomy",
+            "created_at": "2014-02-11T22:55:45.027Z",
+            "updated_at": "2014-02-11T22:55:45.027Z",
+            "clues_count": 10
+        }
+    }, {
+        "id": 16482,
+        "answer": "Honshu",
+        "question": "Kanmon Tunnel, the world's first under an ocean, connects the island of Kyushu with this island",
+        "value": 200,
+        "airdate": "1997-05-09T12:00:00.000Z",
+        "created_at": "2014-02-11T22:55:45.167Z",
+        "updated_at": "2014-02-11T22:55:45.167Z",
+        "category_id": 103,
+        "game_id": null,
+        "invalid_count": null,
+        "category": {
+            "id": 103,
+            "title": "transportation",
+            "created_at": "2014-02-11T22:47:35.305Z",
+            "updated_at": "2014-02-11T22:47:35.305Z",
+            "clues_count": 245
+        }
+    }]
     q.letsplay = function () {
         //API request for 'q.numQuestions' questions
-        questionFactory.getnRandomQuestions(q.numQuestions)
-            .success(function (questions) {
-                q.questions = questions;
-            })
-            .error(function (error) {
-                q.status = 'Unable to load customer data: ' + error.message;
-            });
+        //        questionFactory.getnRandomQuestions(q.numQuestions)
+        //            .success(function (response) {
+        //                q.questions = response;
+        //            })
+        //            .error(function (error) {
+        //                q.status = 'Unable to load customer data: ' + error.message;
+        //            });
 
 
         // show the questions
@@ -36,4 +109,8 @@ quizzer.controller('quizCtrl', function (questionFactory) {
     q.stopplay = function () {
         q.showform = !q.showform
     }
+    q.submit = function () {
+        q.showans = true;
+    }
+
 });

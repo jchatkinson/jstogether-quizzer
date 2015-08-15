f586b0ead296d3c3054fc97f1841e22b1199d966
Jeremy Atkinson <jchatkinson@gmail.com>
2015-08-15 10:35:23 -0700

    added factory to access jservice.io api

--- app.js
+++ app.js
@@ -1,5 +1,39 @@
 var quizzer = angular.module('quizzerApp', []);
 
-quizzer.controller('quizCtrl',function($scope) {
-  $scope.test = 1;
+quizzer.factory('questionFactory', ['$http', function ($http) {
+    var urlBase = 'http://jservice.io/api/random';
+    var dataFactory = {};
+    dataFactory.getRandomQuestion = function () {
+        return $http.get(urlBase);
+    };
+    dataFactory.getnRandomQuestions = function (numQuestions) {
+        return $http.get(urlBase + '?count=' + numQuestions);
+    };
+    return dataFactory;
+}]);
+
+quizzer.controller('quizCtrl', function (questionFactory) {
+    q = this;
+    q.score = 0;
+    q.numQuestions = 4;
+    q.showform = true;
+    q.letsplay = function () {
+        //API request for 'q.numQuestions' questions
+        questionFactory.getnRandomQuestions(q.numQuestions)
+            .success(function (questions) {
+                q.questions = questions;
+            })
+            .error(function (error) {
+                q.status = 'Unable to load customer data: ' + error.message;
+            });
+
+
+        // show the questions
+        q.showform = !q.showform
+    }
+
+
+    q.stopplay = function () {
+        q.showform = !q.showform
+    }
 });
